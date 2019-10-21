498044529c9eee16f1a9d9a537695ecd505e7ce7
Richard Scott Prins Jr <richardsprins@gmail.com>
2019-10-21 01:43:20 -0400

    addded Navbar and styling

--- ecommerce-app/src/Components/Navbar/Navbar.component.jsx
+++ ecommerce-app/src/Components/Navbar/Navbar.component.jsx
@@ -1,13 +1,23 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom'
+import { ReactComponent as Logo  } from '../../assets/crown.svg'
+import './Navbar.styles.scss'
  
 export default class Navbar extends Component {
   render() {
     return (
-      <div>
-        <Link to="/shop">
-          <h1>Crown</h1>
+      <div className="header">
+        <Link className="logo-container" to="/">
+          <Logo className="logo"/>
         </Link>
+        <div className="options">
+          <Link className="option" to ="/shop">
+            <h3>SHOP</h3>
+          </Link>
+          <Link className="option" to ="/contact">
+            <h3>CONTACT</h3>
+          </Link>
+        </div>
       </div>
     )
   }
